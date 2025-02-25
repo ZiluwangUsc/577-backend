607e802866199b2736358ce2f27b0d055715d397
ssklin <shengkuo.lin@outlook.com>
2025-02-24 16:49:38 -0800

    add some message string constants

--- src/main/java/com/tripwise/backend/constants/Constants.java
+++ src/main/java/com/tripwise/backend/constants/Constants.java
@@ -3,4 +3,8 @@ package com.tripwise.backend.constants;
 public final class Constants {
     public static final Integer TOKEN_EXPIRE_TIME = 3600; // unit: seconds, equivalent to 1 hour
     public static final String LOG_OUT_OK = "User logged out successfully";
+    public static final String REGISTER_OK = "User registered successfully";
+    public static final String REGISTER_EXISTED = "User already exists";
+    public static final String LOGIN_FAIL = "Invalid email or password";
+    public static final String LOGIN_OK = "User logged in";
 }
